5f003511489a05ca3b2e01bdad78e19b66f169ef
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-12-20 14:33:38 +0000

    opensidebar function created

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function openSideBar(){
+ const sidebarElement = document.getElementById('sidebar');
+ sidebarElement.style.visibility = 'visible';
+
+ const hambergerMenuElement = document.getElementById('hamberger-menu');
+ hambergerMenuElement.style.visibility = 'hidden';
+}
+
+function closeSideBar(){
+    
+}
